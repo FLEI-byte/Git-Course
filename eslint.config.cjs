e3c55a16a6da4a388f6dba3731e4f4664967966c
Vlada <omurise38@gmail.com>
2026-09-23 14:37:16 +0300

    chore: stabilize public API; forbid any; add package exports

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,7 +3,7 @@ const tseslint = require("typescript-eslint");
 const prettier = require("eslint-config-prettier");
 
 module.exports = [
-  { ignores: ["**/*.cjs"] },
+  { ignores: ["**/*.cjs", "dist/**"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
@@ -21,7 +21,7 @@ module.exports = [
     },
 
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "error",
       "no-unused-vars": "warn",
     },
   },
